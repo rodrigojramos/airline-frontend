169b6e8d8cf557976c613a03ad8a36b1103a60dc
Rodrigo Ramos <digojramos@gmail.com>
2024-08-30 10:31:52 -0300

    DeleteById Flight

--- src/components/AllFlightsCard/index.tsx
+++ src/components/AllFlightsCard/index.tsx
@@ -1,6 +1,10 @@
 import { MoveRight, NotebookText, Pencil, Trash2 } from "lucide-react";
 import { Link } from "react-router-dom";
 import { FlightDTO } from "../../models/flight";
+import { useState } from "react";
+import * as flightService from '../../services/flight-service';
+import { DialogInfo } from "../DialogInfo";
+import { DialogConfirmation } from "../DialogConfirmation";
 
 type Props = {
     flight: FlightDTO;
@@ -8,33 +12,91 @@ type Props = {
 
 export function AllFlightsCard({ flight }: Props) {
 
+    const [dialogInfoData, setDialogInfoData] = useState({
+        visible: false,
+        message: "Mensagem"
+    })
+
+    const [dialogConfirmationData, setDialogConfirmationData] = useState({
+        visible: false,
+        id: 0,
+        message: "Tem certeza?"
+    })
+
+    function handleDialogConfirmationAnswer(answer: boolean, flightId: number) {
+
+        if(answer) {
+            flightService.deleteById(flightId)
+            .then(() => {
+                window.location.reload();
+            })
+            .catch(() => {
+                setDialogInfoData({
+                    visible: true,
+                    message: " Falha de intregridade referencial"
+                })
+            })
+        }
+
+        setDialogConfirmationData({ ...dialogConfirmationData, visible: false});
+    }
+
+    function handleDeleteClick(flightId: number) {
+        setDialogConfirmationData({ ...dialogConfirmationData, id: flightId, visible: true});
+    }
+
+    function handleDialogInfoClose() {
+        setDialogInfoData({...dialogInfoData, visible: false});
+    }
+
     const date = new Date(flight.flightDay);
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const time = `${hours}:${minutes}`;
 
     return (
-        <div className="airline-all-flights-card">
-            <div className="airline-all-flights-destination">
-                <p className="padding-right-10">{flight.departure}</p>
-                <MoveRight />
-                <p className="padding-left-10">{flight.arrival}</p>
+        <section>
+            <div className="airline-all-flights-card">
+                <div className="airline-all-flights-destination">
+                    <p className="padding-right-10">{flight.departure}</p>
+                    <MoveRight />
+                    <p className="padding-left-10">{flight.arrival}</p>
+                </div>
+                <p>Horário: {time}</p>
+                <p>Lugares livres: {flight.availableSeats}</p>
+                <div className="airline-all-flights-card-icons">
+                    <div className="airline-all-flights-card-icon">
+                        <Link to="/admin/passenger-list">
+                            <NotebookText />
+                        </Link>
+                    </div>
+                    <div className="airline-all-flights-card-icon">
+                        <Pencil />
+                    </div>
+                    <div className="airline-all-flights-card-icon">
+                        <Trash2 onClick={() => handleDeleteClick(flight.id)}/>
+                    </div>
+                </div>
             </div>
-            <p>Horário: {time}</p>
-            <p>Lugares: {flight.availableSeats}/180</p>
-            <div className="airline-all-flights-card-icons">
-                <div className="airline-all-flights-card-icon">
-                    <Link to="/admin/passenger-list">
-                        <NotebookText />
-                    </Link>
-                </div>
-                <div className="airline-all-flights-card-icon">
-                    <Pencil />
-                </div>
-                <div className="airline-all-flights-card-icon">
-                    <Trash2 />
-                </div>
-            </div>
-        </div>
+
+            {
+                dialogInfoData.visible &&
+                <DialogInfo 
+                    message={dialogInfoData.message}
+                    onDialogClose={handleDialogInfoClose}
+                />
+            }
+            
+            {
+                dialogConfirmationData.visible &&
+                <DialogConfirmation
+                    message={dialogConfirmationData.message}
+                    id={dialogConfirmationData.id}
+                    onDialogAnswer={handleDialogConfirmationAnswer}
+                />
+            }
+
+        </section>
+        
     )
 }
