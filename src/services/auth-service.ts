88d271b9dee125a1cbcd3839a3c2b63580956c5d
Rodrigo Ramos <digojramos@gmail.com>
2024-09-03 10:33:52 -0300

    isAuthenticated

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,9 +1,10 @@
 import QueryString from "qs";
-import { CredentialsDTO } from "../models/auth";
+import { AccessTokenPayloadDTO, CredentialsDTO } from "../models/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
 import * as acessTokenRepository from "../localstorage/acess-token-repository"
+import jwtDecode from "jwt-decode";
 
 export function loginRequest(loginData: CredentialsDTO) {
     
@@ -34,4 +35,25 @@ export function saveAccessToken(token: string) {
 
 export function getAccessToken() {
    return acessTokenRepository.get();
+}
+
+export function getAccessTokenPayload(): AccessTokenPayloadDTO | undefined {
+    try {
+        const token = acessTokenRepository.get();
+        return token == null
+            ? undefined
+            : (jwtDecode(token) as AccessTokenPayloadDTO);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    catch (error) {
+        return undefined;
+    }
+}
+
+export function isAuthenticated(): boolean {
+    const tokenPayload = getAccessTokenPayload();
+    if (tokenPayload && tokenPayload.exp * 1000 > Date.now()) {
+        return true;
+    }
+    return false;
 }
